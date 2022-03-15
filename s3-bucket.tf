76a11c30ca9f1c344a9f0c8b2c4c8445f63bc5de
Brendon Connors <brendon.connors@csa.com.au>
2022-03-15 10:06:29 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "s3bucket-gaurav123lolfml456"
+  bucket = "s3bucket-gaurav123lolfm22l456"
   acl    = "private"
 
 }
